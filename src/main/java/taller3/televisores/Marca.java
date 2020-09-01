d63815e883b4543f7e4c84ceec34d524d7eb1112
DeninsonCH <69688198+DeninsonCH@users.noreply.github.com>
2020-09-01 11:38:06 -0500

    publicMarca
    Agrego public a marca y  public tv

--- src/main/java/taller3/televisores/Marca.java
+++ src/main/java/taller3/televisores/Marca.java
@@ -2,7 +2,14 @@ package taller3.televisores;
 
 public class Marca {
 	String nombre;
-	Marca (String nombre){
-		nombre = nombre;
+	public Marca (String tex){
+		nombre=tex;
 	}
+	public String getNombre () {
+		return nombre;
+	}
+	public void setNombre (String nn) {
+		nombre=nn;
+	}
+	
 }
--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -1,21 +1,88 @@
 package taller3.televisores;
 
 public class TV {
-	private Marca marca; 
-	private int canal = 1; 
-	private int precio = 500; 
-	private boolean estado = false;
-	private int volumen = 1;
-	private Control control;
-	private static int numTV=0;
-	
-	public TV(Marca marca, boolean estado) {
-		this.setMarca(marca);
-		this.estado = estado;
-		setnumTV(getNumTV()+1);
+	Marca marca;
+	public int canal=1, precio=500, volumen=1;
+	public boolean estado;	
+	public Control control;
+	static int numTV=0;
+	public TV (Marca m,boolean e){
+		this.marca=m;
+		estado=e;
+		numTV++;
 	}
-	
-	public turnOn() {
+	public static void setNumTV (int n) {
+		numTV = n;
+	}
+	public static int getNumTV() {
+		return numTV;
+	}
+
+	public Marca getMarca () {
+		return this.marca;
+    }
+	public Control getControl () {
+		return this.control;
+    }
+	public int getPrecio () {
+		return precio;
+    }
+	public int getVolumen () {
+		return volumen;
+    }
+	public int getCanal () {
+		return canal;
+    }
+	public boolean getEstado() {
+		return estado;
+	}
+	public void setMarca (Marca mar) {
+		this.marca=mar;
+	}
+	public void setControl (Control con) {
+		this.control=con;
+	}
+	public void setPrecio (int pre) {
+		precio=pre;
+	}
+	public void setVolumen (int vol) {
+		if((vol>=0) && (vol<=7)){
+			volumen=vol;
+		}
 		
 	}
+	public void setCanal (int can) {
+		if ((can<=120)  && (estado==true) && (can>=1)){
+			canal=can;
+		}
+	}
+	public void turnOn () {
+		estado=true;
+	}
+	public void turnOff () {
+		estado=false;
+	}
+	public void canalUp () {
+		if ((canal<120) && (canal>=1) && (estado==true)) {
+		canal++;
+	}
+	}
+	public void canalDown () {
+		if ((canal>1)&& (canal<=120) && (estado==true)) {
+		canal--;
+		}
+	}
+	public void volumenUp () {
+		if ((volumen<7) && (volumen>=0) && (estado==true)) {
+		volumen++;
+		}
+	}
+	public void volumenDown () {
+		if ((volumen>0) && (volumen<7) && (estado==true)) {
+		volumen--;
+		}
+	}
+
+
+
 }
